587b961a9545db20b84a2546205df4584b679c56
Payton Shaltis <shaltisp@gmail.com>
2021-08-08 11:37:46 -0400

    refactored _draw_grid() into its own method

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 """Contains the Game class to create instances of the New Game of Life."""
-import pygame, pygame.display, pygame.event, pygame.rect
+import pygame, pygame.display, pygame.event, pygame.rect, pygame.draw
 import sys
 from settings import Settings
 
 class Game:
     """An instance of the Game class."""
-    
+
     def __init__(self):
         """Creates an instance of the Game class"""
+        
+        # instance variables
+        self.settings = None
+        self.screen = None
+        
+        # initialize Pygame 
         pygame.init()
 
         # create a Settings object
@@ -15,7 +21,37 @@ class Game:
 
         # set up the screen
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen.fill(self.settings.bg_color)
         pygame.display.set_caption('New Game of Life')
+
+        # create the grid
+        self._draw_grid()
+        
+    def _draw_grid(self):
+        """Draws the grid on the main screen surface."""
+        
+        for x in range(self.settings.screen_width):
+            for y in range(self.settings.screen_height):
+                
+                # create a temporary Rect to be drawn
+                rect = pygame.Rect(
+                    x * self.settings.square_size,
+                    y * self.settings.square_size,
+                    self.settings.square_size,
+                    self.settings.square_size
+                )
+
+                # draw the temp Rect with proper settings
+                pygame.draw.rect(
+                    self.screen, 
+                    self.settings.square_color, 
+                    rect, 
+                    self.settings.square_bord_thick
+                )
+
+
+
+        
 
     def run_game(self):
         """The main game loop."""
--- settings.py
+++ settings.py
@@ -9,13 +9,16 @@ class Settings:
     def __init__(self, game):
         """Creates an instance of the Settings class using a Game instance."""
         
+        # instance variables
         # instance of the Game class for access to its attributes
         self.game = game
 
         # screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = COLOR_BLACK
+        self.bg_color = COLOR_WHITE
         
         # square settings
-        self.square_color = COLOR_WHITE
+        self.square_color = COLOR_BLACK
+        self.square_size = 30
+        self.square_bord_thick = 1

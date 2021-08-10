2f1655b82c22a4ba6a7b8e12c3df00cd6777aa70
Payton Shaltis <shaltisp@gmail.com>
2021-08-10 17:30:03 -0400

    controls have started being added to the menu

--- menu.py
+++ menu.py
@@ -15,7 +15,10 @@ class Menu:
         self.menu_rect = None
 
         # menu elements
+        self.all_menu_items = []
         self.element_title = None
+        self.element_controls = None
+        self.element_control1 = None
 
         # initialize the menu (only done once)
         self._initialize_menu()
@@ -32,7 +35,8 @@ class Menu:
         )
 
         # the menu elements
-        self.element_title.draw_menu_element()
+        for element in self.all_menu_items:
+            element.draw_menu_element()
 
 
     def _initialize_menu(self):
@@ -55,4 +59,32 @@ class Menu:
                 (self.settings.screen_width * self.settings.screen_height) / 12500),
             color=(0, 0, 0), 
             position=self.menu_rect.midtop,
-            bold=False)
+            anchor='midtop'
+        )
+        self.element_controls = MenuElement(
+            game=self.game,
+            text='Controls:',
+            font=self.settings.font,
+            font_size=int(
+                (self.settings.screen_width * self.settings.screen_height) / 25000),
+            color=(0, 0, 0),
+            position=self.menu_rect.midleft,
+            anchor='midleft',
+            deltax=int((1/40) * self.size[0]),
+            deltay= -int((1/4) * self.size[1])
+        )
+        self.element_control1 = MenuElement(
+            game=self.game,
+            text='\'Enter\' - begin simulation',
+            font=self.settings.font,
+            font_size=int(
+                (self.settings.screen_width * self.settings.screen_height) / 40000),
+            color=(0, 0, 0),
+            position=self.element_controls.rect.bottomleft,
+            anchor='topleft',
+        )
+
+        # append all elements to the list
+        self.all_menu_items.append(self.element_title)
+        self.all_menu_items.append(self.element_controls)
+        self.all_menu_items.append(self.element_control1)
--- menu_element.py
+++ menu_element.py
@@ -4,7 +4,7 @@ import pygame, pygame.font
 class MenuElement:
     """Instance of a single element in the menu."""
 
-    def __init__(self, game, text, font, font_size, color, position, bold):
+    def __init__(self, game, text, font, font_size, color, position, anchor, deltax=0, deltay=0, bold=False):
         """Creates a new MenuElement from the given parameters."""
 
         # instance variables
@@ -13,6 +13,9 @@ class MenuElement:
         self.color = color
         self.position = position
         self.bold = bold
+        self.anchor = anchor
+        self.deltax = deltax
+        self.deltay = deltay
         self.font = pygame.font.SysFont(font, font_size, bold=self.bold)
         self.rect = None
         self.text_image = None
@@ -22,7 +25,15 @@ class MenuElement:
 
         # build the element's Rect object and position it
         self.rect = self.text_image.get_rect()
-        self.rect.midtop = self.position 
+        if self.anchor == 'midtop':
+            self.rect.midtop = self.position
+        elif self.anchor == 'midleft':
+            self.rect.midleft = self.position
+        elif self.anchor == 'topleft':
+            self.rect.topleft = self.position
+
+        self.rect.x += deltax
+        self.rect.y += deltay
 
     
     def draw_menu_element(self):
